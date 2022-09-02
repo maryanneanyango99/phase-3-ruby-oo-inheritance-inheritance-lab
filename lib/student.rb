7709925e4643fae441b882016a62e38208d6dedb
Moringa School <maryanneanyango98@gmail.com>
2022-09-02 16:31:03 +0300

    commit tree 19e3eaf83913e1529dcffe904d49295432ba8758

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
-end
+class Student < User
+    def initialize
+        @knowledge=[]
+    end
+ 
+    def learn (string)
+        @knowledge << string
+    end
+ 
+    def knowledge
+        @knowledge
+    end
+ end
